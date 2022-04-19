c036c05ec6b7a3a5d3448cffce93fbcb49e38ac8
Olive Hehemann <Oliveavalinh@gmail.com>
2022-04-19 07:47:47 -0700

    Made menu display in guest form

--- Documentation/Milestone 6/We Serve U/We Serve U/loginForm.cpp
+++ Documentation/Milestone 6/We Serve U/We Serve U/loginForm.cpp
@@ -4,7 +4,7 @@
 using namespace System;
 using namespace System::IO;
 using namespace System::Windows::Forms;
-void main(array<String^>^ args)
+void main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
